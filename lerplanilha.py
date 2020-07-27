845ecf261fa80eba4e0e1cda09b44677fa3aeb7e
Guilherme <guirlviana@gmail.com>
2020-07-27 14:35:57 -0300

    first commit

---
+++ lerplanilha.py
@@ -0,0 +1,19 @@
+def consulta(idcliente=0):
+    import openpyxl
+
+    planilha = openpyxl.load_workbook('Clientes 2.xlsm')
+    planilha_clientes = planilha['Clientes']
+    cliente = idcliente
+    c = 11
+    for linha in planilha_clientes.iter_rows(min_row= 11, min_col=3, max_col=3):    
+        for cedula in linha:        
+            if cedula.value == cliente:            
+                print(f"ID: {cedula.value}")
+                print(f"NOME: {planilha_clientes[f'D{c}'].value}")
+                print(f"TEL: {planilha_clientes[f'E{c}'].value}")
+                print(f"ENDEREÇO: {planilha_clientes[f'F{c}'].value}")
+            else:
+                c += 1
+            
+if __name__ == "__main__":
+    consulta(103)    
