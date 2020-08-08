2cc2b3d2e73b31c4449328df4827dde5e0d3944c
Guilherme <guirlviana@gmail.com>
2020-08-08 20:47:33 -0300

    adicionado modularização

--- gerenciador.py
+++ gerenciador.py
@@ -1,107 +1,106 @@
 import openpyxl
-def consulta():
-    planilha = openpyxl.load_workbook('Clientes 2.xlsx')
-    planilha_clientes = planilha['Clientes']    
-    allid = list()
-    for linha in planilha_clientes.iter_rows(min_row= 11, min_col=3, max_col=3):    
-        for cedula in linha:
-            allid.append(cedula.value)
-    allid.remove(None)
-    cliente = int(input("Qual ID cliente: "))
-    if cliente not in allid:
-      print("Cliente nao encontrado")
-      allid.clear()
-      consulta()
-    else:
-        verDados(cliente, planilha_clientes)
 
-def verDados(cliente, planilha_clientes):
-    c = 11
-    for linha in planilha_clientes.iter_rows(min_row= 11, min_col=3, max_col=3):    
-        for cedula in linha:        
-            if cedula.value == cliente: 
-                print('=-' * 20)         
-                print(f"ID: {cedula.value}")
-                print(f"NOME: {planilha_clientes[f'D{c}'].value}")
-                tel = planilha_clientes[f'E{c}'].value
-                if len(tel) == 11:                    
-                    print(f'TEL: ({tel[0]}{tel[1]}) {tel[2:6]}-{tel[6:]}')
-                else:
-                    print(f"TEL: {planilha_clientes[f'E{c}'].value}")
-                print(f"END: {planilha_clientes[f'F{c}'].value}")
-                print('=-' * 20) 
-            else:
-                c += 1
+class Manager():
+    def __init__(self):
+        self.planilha = openpyxl.load_workbook('Clientes test.xlsx')
+        self.planilha_clientes = self.planilha['Clientes'] 
 
-def pegarDados():
-    while True:
-        nome = str(input('NOME: '))
-        if nome not in '':
-            break
-    while True:    
-        tel = str(input('TEL: '))
-        if tel not in '':
-            break
-    while True:    
-        end = str(input('END: ')) 
-        if end not in '':
-            break
-    cadastar(nome, tel, end)
-
-
-def cadastar(nome, tel, end):
-    planilha = openpyxl.load_workbook('Clientes 2.xlsx')
-    planilha_clientes = planilha['Clientes']   
-    allid = list()
-    for linha in planilha_clientes.iter_rows(min_row= 11, min_col=3, max_col=3):    
-        for cedula in linha:
-            if cedula.value == None:
-                pass
-            else:
+    def consulta(self):
+        allid = list()
+        for linha in self.planilha_clientes.iter_rows(min_row= 11, min_col=3, max_col=3):    
+            for cedula in linha:
                 allid.append(cedula.value)
-    
-    allid.sort()
-    idusuario = allid[-1] +1
-    linha_dado = len(allid) +11  
-    print('=-' * 20)
-    print(f'Adicionar id no contato = {idusuario}')
-    print('=-' * 20)
-    planilha_clientes[f'C{linha_dado}'] = idusuario
-    planilha_clientes[f'D{linha_dado}'] = nome
-    planilha_clientes[f'E{linha_dado}'] = tel
-    planilha_clientes[f'F{linha_dado}'] = end
-    planilha.save('Clientes 2.xlsx')
+        allid.remove(None)
+        cliente = int(input("Qual ID cliente: "))
+        if cliente not in allid:
+            print("Cliente nao encontrado")
+            allid.clear()
+            self.consulta()
+        else:
+            self.verDados(cliente)
 
+    def verDados(self, cliente):
+        c = 11
+        for linha in self.planilha_clientes.iter_rows(min_row= 11, min_col=3, max_col=3):    
+            for cedula in linha:        
+                if cedula.value == cliente: 
+                    print('=-' * 20)         
+                    print(f"ID: {cedula.value}")
+                    print(f"NOME: {self.planilha_clientes[f'D{c}'].value}")                 
+                    print(f"TEL: {self.planilha_clientes[f'E{c}'].value}")
+                    print(f"END: {self.planilha_clientes[f'F{c}'].value}")
+                    print('=-' * 20) 
+                else:
+                    c += 1
 
-def menu():
-    import os
-    print('   CLIENTES HOMEBURGUER')
-    print('=-' * 20)
-    
-    while True:
-        escolha = str(input('[1] BUSCAR CLIENTE\n[2] CADASTRAR CLIENTE\n[3] LIMPAR TELA\n[4] SAIR\nESCOLHA: '))
-        if escolha in '1234':
-            break
-    if escolha == '1':
-        consulta()        
-        menu()
-    if escolha == '2':
-        pegarDados()        
-        menu()
-    if escolha == '3':
-        os.system('cls')
-        menu()
-    if escolha == '4':
+    def pegarDados(self):
         while True:
-            decisão = str(input('Deseja sair? [S/N] ')).strip().upper()[0]
-            if decisão in 'SN':
+            nome = str(input('NOME: '))
+            if nome not in '':
                 break
-        if decisão == 'S':  
-            os.system('exit')
-        elif decisão == 'N':
-            print()
-            menu()    
+        while True:    
+            tel = str(input('TEL: '))
+            if tel not in '':
+                break
+        while True:    
+            end = str(input('END: ')) 
+            if end not in '':
+                break
+        self.cadastar(nome, tel, end)
+
+
+    def cadastar(self, nome, tel, end):
+           
+        allid = list()
+        for linha in self.planilha_clientes.iter_rows(min_row= 11, min_col=3, max_col=3):    
+            for cedula in linha:
+                if cedula.value == None:
+                    pass
+                else:
+                    allid.append(cedula.value)
+        
+        allid.sort()
+        idusuario = allid[-1] +1
+        linha_dado = len(allid) +11  
+        print('=-' * 20)
+        print(f'Adicionar id no contato = {idusuario}')
+        print('=-' * 20)
+        self.planilha_clientes[f'C{linha_dado}'] = idusuario
+        self.planilha_clientes[f'D{linha_dado}'] = nome
+        self.planilha_clientes[f'E{linha_dado}'] = tel
+        self.planilha_clientes[f'F{linha_dado}'] = end
+        self.planilha.save('Clientes test.xlsx')
+
+
+    def menu(self):
+        import os
+        print('   CLIENTES HOMEBURGUER')
+        print('=-' * 20)
+        
+        while True:
+            escolha = str(input('[1] BUSCAR CLIENTE\n[2] CADASTRAR CLIENTE\n[3] LIMPAR TELA\n[4] SAIR\nESCOLHA: '))
+            if escolha in '1234':
+                break
+        if escolha == '1':
+            self.consulta()        
+            self.menu()
+        if escolha == '2':
+            self.pegarDados()        
+            self.menu()
+        if escolha == '3':
+            os.system('cls')
+            self.menu()
+        if escolha == '4':
+            while True:
+                decisão = str(input('Deseja sair? [S/N] ')).strip().upper()[0]
+                if decisão in 'SN':
+                    break
+            if decisão == 'S':  
+                os.system('exit')
+            elif decisão == 'N':
+                print()
+                self.menu()    
 
 if __name__ == "__main__":
-    print()
-    menu()
+    system = Manager()
+    system.menu()
